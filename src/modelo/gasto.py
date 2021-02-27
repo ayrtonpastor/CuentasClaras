a2893cce3d68bf063b35023f6aeaa08254ac8af3
Pedro Santiago Lizarazo <ps.lizarazo@uniandes.edu.co>
2021-02-26 23:13:19 -0500

    creacion modelo gasto

---
+++ src/modelo/gasto.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Numeric, ForeignKey, Date
+from .declarative_base import Base
+
+
+class Gasto(Base):
+    __tablename__ = 'gasto'
+
+    id = Column(Integer, primary_key=True)
+    concepto = Column(String)
+    monto = Column(Numeric)
+    fecha = Column(Date)
+
+    actividad_id = Column(Integer, ForeignKey('actividad.id'), nullable=False)
+    viajero_id = Column(Integer, ForeignKey('viajero.id'), nullable=False)
