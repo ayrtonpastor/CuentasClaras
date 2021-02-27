77a15c6e92a0ebc615d1c800d42086c21d5832e6
Pedro Santiago Lizarazo <ps.lizarazo@uniandes.edu.co>
2021-02-27 12:20:59 -0500

    Paso 2: refactor Listar gastos por actividad(Rojo)

--- tests/test_gasto.py
+++ tests/test_gasto.py
@@ -7,41 +7,65 @@ from src.modelo.gasto import Gasto
 from src.modelo.declarative_base import Session
 from datetime import date
 
+
 class ActividadTestCase(unittest.TestCase):
     def setUp(self):
         self.control_cuenta = ControlCuenta()
         self.session = Session()
 
-        self.actividad1 = Actividad(nombre="Actividad de Ayrton", terminada=False)
-        self.actividad2 = Actividad(nombre="Actividad de Pedro", terminada=False) 
-        self.actividad3 = Actividad(nombre="Actividad de Raul", terminada=False)
-        self.actividad4 = Actividad(nombre="Actividad de Dario", terminada=False)
+        self.actividad1 = Actividad(
+            nombre="Actividad de Ayrton", terminada=False
+        )
+        self.actividad2 = Actividad(
+            nombre="Actividad de Pedro", terminada=False
+        )
+        self.actividad3 = Actividad(
+            nombre="Actividad de Raul", terminada=False
+        )
 
-        self.session.add_all([self.actividad1, self.actividad2, self.actividad3, self.actividad4])
+        self.session.add_all(
+            [self.actividad1, self.actividad2, self.actividad3])
 
         self.viajero1 = Viajero(nombre="Dario", apellido="Correal")
         self.viajero2 = Viajero(nombre="Ayrton", apellido="Pastor")
         self.viajero3 = Viajero(nombre="Pedro", apellido="Lizarazo")
         self.viajero4 = Viajero(nombre="Raul", apellido="Calero")
 
-        self.session.add_all([self.viajero1, self.viajero2, self.viajero3, self.viajero4])
+        self.session.add_all(
+            [self.viajero1, self.viajero2, self.viajero3, self.viajero4])
         self.session.flush()
 
-        self.actividad_viajero1 = ActividadViajero(actividad_id=self.viajero1.id, viajero_id=self.actividad1.id)
-        self.actividad_viajero2 = ActividadViajero(actividad_id=self.viajero2.id, viajero_id=self.actividad1.id)
-        self.actividad_viajero3 = ActividadViajero(actividad_id=self.viajero3.id, viajero_id=self.actividad1.id)
-        self.actividad_viajero4 = ActividadViajero(actividad_id=self.viajero4.id, viajero_id=self.actividad1.id)
+        # Viajeros asociados a actividad 1
+        self.actividad_viajero1 = ActividadViajero(
+            actividad_id=self.actividad1.id, viajero_id=self.viajero1.id)
+        self.actividad_viajero2 = ActividadViajero(
+            actividad_id=self.actividad1.id, viajero_id=self.viajero2.id)
 
-        self.session.add_all([self.actividad_viajero1, self.actividad_viajero2, self.actividad_viajero3, self.actividad_viajero4])
+        # Viajeros asociados a actividad 2
+        self.actividad_viajero3 = ActividadViajero(
+            actividad_id=self.actividad2.id, viajero_id=self.viajero3.id)
+        self.actividad_viajero4 = ActividadViajero(
+            actividad_id=self.actividad2.id, viajero_id=self.viajero4.id)
 
+        self.session.add_all([self.actividad_viajero1, self.actividad_viajero2, self.actividad_viajero3,
+                              self.actividad_viajero4])
+
+        # Gastos asociados a la actividad 1
         self.gasto1 = Gasto(concepto="paletas heladas", monto=1234, fecha=date(2021, 1, 1),
                             viajero_id=self.viajero1.id, actividad_id=self.actividad1.id)
         self.gasto2 = Gasto(concepto="alcohol", monto=4444, fecha=date(2021, 1, 4),
-                            viajero_id=self.viajero3.id, actividad_id=self.actividad1.id)
-        self.session.add_all([self.gasto1, self.gasto2])
+                            viajero_id=self.viajero2.id, actividad_id=self.actividad1.id)
+
+        # Gastos asociados a la actividad 2
+        self.gasto3 = Gasto(concepto="viaje de despedida", monto=999.24, fecha=date(
+            2020, 1, 1), viajero_id=self.viajero3.id, actividad_id=self.actividad2.id)
+
+        self.session.add_all([self.gasto1, self.gasto2, self.gasto3])
 
         self.session.commit()
         self.actividad1_id = self.actividad1.id
+        self.actividad2_id = self.actividad2.id
+        self.actividad3_id = self.actividad3.id
         self.session.close()
 
     def tearDown(self):
@@ -62,9 +86,17 @@ class ActividadTestCase(unittest.TestCase):
 
         self.session.commit()
         self.session.close()
-    
+
     def test_listar_gastos(self):
-        gastos = self.control_cuenta.listarGastos(self.actividad1_id)
-        self.assertEqual(len(gastos), 2)
-
-
+        gastos_actividad1 = self.control_cuenta.listarGastos(
+            self.actividad1_id
+        )
+        gastos_actividad2 = self.control_cuenta.listarGastos(
+            self.actividad2_id
+        )
+        gastos_actividad3 = self.control_cuenta.listarGastos(
+            self.actividad3_id
+        )
+        self.assertEqual(len(gastos_actividad1), 2)
+        self.assertEqual(len(gastos_actividad2), 1)
+        self.assertEqual(len(gastos_actividad3), 0)
