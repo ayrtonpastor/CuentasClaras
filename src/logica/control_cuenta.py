f2ecc37ff4a8406f60ac8406adecb6af7a4a8df8
Ayrton <a.pastor@uniandes.edu.co>
2021-02-27 22:20:31 -0500

    Paso 3:Reporte compensacion ac sin viajero(Verde)

--- src/logica/control_cuenta.py
+++ src/logica/control_cuenta.py
@@ -1,5 +1,5 @@
 from src.modelo.declarative_base import engine, Base, session
-from src.modelo.actividad import Actividad
+from src.modelo.actividad import Actividad, ActividadViajero
 from src.modelo.viajero import Viajero
 from src.modelo.gasto import Gasto
 
@@ -13,7 +13,9 @@ class ControlCuenta():
         return session.query(Actividad).all()
 
     def crearReporteCompensacion(self, actividad_id):
-        pass
+        viajeros = session.query(ActividadViajero).filter_by(actividad_id=actividad_id)
+        if viajeros.count() == 0:
+            return []
 
     def listarGastos(self, actividad_id):
         actividad = session.query(Actividad).filter_by(id=actividad_id).first()
