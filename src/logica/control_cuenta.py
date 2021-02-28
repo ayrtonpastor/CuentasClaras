756b7321603361f699ef9a9f07d4db212bfeb466
Pedro Santiago Lizarazo <ps.lizarazo@uniandes.edu.co>
2021-02-28 12:05:44 -0500

    Eliminacion de datos en logicamock

--- src/logica/control_cuenta.py
+++ src/logica/control_cuenta.py
@@ -11,8 +11,6 @@ class ControlCuenta():
     def __init__(self):
         # TODO: Remover datos de prueba del Constructor a medida que se desarrollan las historias de usuario
         self.actividades = ["Actividad 1", "Actividad 2", "Actividad 3"]
-        self.viajeros = [{"Nombre":"Pepe", "Apellido":"Pérez"}, {"Nombre":"Ana", "Apellido":"Andrade"}]
-        self.gastos = [{"Concepto":"Gasto 1", "Fecha": "12-12-2020", "Valor": 10000, "Nombre": "Pepe", "Apellido": "Pérez"}, {"Concepto":"Gasto 2", "Fecha": "12-12-2020", "Valor": 20000, "Nombre":"Ana", "Apellido":"Andrade"}]
         self.matriz = [["", "Pepe Pérez", "Ana Andrade", "Pedro Navajas" ],["Pepe Pérez", -1, 1200, 1000],["Ana Andrade", 0, -1, 1000], ["Pedro Navajas", 0, 0, -1]]
         self.gastos_consolidados = [{"Nombre":"Pepe", "Apellido":"Pérez", "Valor":15000}, {"Nombre":"Ana", "Apellido":"Andrade", "Valor":12000},{"Nombre":"Pedro", "Apellido":"Navajas", "Valor":0}]
         self.viajeros_en_actividad = [{"Nombre": "Pepe Pérez", "Presente":True}, {"Nombre": "Ana Andrade", "Presente":True}, {"Nombre":"Pedro Navajas", "Presente":False}]
