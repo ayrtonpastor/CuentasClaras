0017c18fd5fc517eba1a4af6066d20aaf825bdb8
Ayrton <a.pastor@uniandes.edu.co>
2021-02-27 23:47:16 -0500

    Paso 4: Reporte compensacion sin gastos (Verde)

--- src/logica/control_cuenta.py
+++ src/logica/control_cuenta.py
@@ -1,3 +1,5 @@
+from sqlalchemy import func
+
 from src.modelo.declarative_base import engine, Base, session
 from src.modelo.actividad import Actividad, ActividadViajero
 from src.modelo.viajero import Viajero
@@ -13,9 +15,26 @@ class ControlCuenta():
         return session.query(Actividad).all()
 
     def crearReporteCompensacion(self, actividad_id):
-        viajeros = session.query(ActividadViajero).filter_by(actividad_id=actividad_id)
-        if viajeros.count() == 0:
+        actividad_viajeros = session.query(ActividadViajero).filter_by(actividad_id=actividad_id)
+        if actividad_viajeros.count() == 0:
             return []
+        else:
+            objetos = []
+            for actividad_viajero in actividad_viajeros:
+                gastos_por_viajero = session.query(func.sum(Gasto.monto)).filter_by(actividad_id=actividad_id,
+                                                                                    viajero_id=actividad_viajero.viajero_id).first()
+                monto_gasto_por_viajero = gastos_por_viajero[0] if gastos_por_viajero[0] else 0
+
+                viajero = session.query(Viajero).filter_by(id=actividad_viajero.viajero_id).first()
+                promedio = monto_gasto_por_viajero / actividad_viajeros.count()
+
+                objeto = {
+                    "nombre": viajero.nombre + ' ' + viajero.apellido,
+                    "monto_debe_cada_uno": "{:.2f}".format(promedio)
+                }
+                objetos.append(objeto)
+        print(objetos)
+        return objetos
 
     def listarGastos(self, actividad_id):
         actividad = session.query(Actividad).filter_by(id=actividad_id).first()
