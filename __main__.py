6e4a739c3ea6abf4af92bd1e1b5da2eae38483bc
Pedro Santiago Lizarazo <ps.lizarazo@uniandes.edu.co>
2021-02-28 19:34:56 -0500

    Remove ToDo

--- __main__.py
+++ __main__.py
@@ -2,13 +2,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from src.vista.Interfaz_CuentasClaras import App_CuentasClaras
 from src.logica.control_cuenta import ControlCuenta
-from src.mockdata.mockdata import MockDataFactory
+from src.mockdata.mockdata import MockDataFactory   
 
 if __name__ == '__main__':
     #Punto inicial de la aplicación 
 
     control_cuenta = ControlCuenta()
-    #TODO: Crear archivo que incluya datos mock en la base de datos
 
     factory_mockup_data = MockDataFactory()
     factory_mockup_data.setUp()
