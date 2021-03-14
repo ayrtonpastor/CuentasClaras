ec0ebe2ff5c9d4fc924476e78bba01e6890b2485
Ayrton <a.pastor@uniandes.edu.co>
2021-03-14 00:08:52 -0500

    Corrección de validación de gasto que impide crear gastos con mismo concepto (no esta en los criterios de aceptación)

--- src/logica/control_cuenta.py
+++ src/logica/control_cuenta.py
@@ -129,30 +129,31 @@ class ControlCuenta():
         else:
             actividad = session.query(Actividad).filter_by(id=actividad_id).first()
             viajero = session.query(Viajero).filter_by(id=viajero_id).first()
-            actividad_viajero = session.query(ActividadViajero).filter_by(actividad_id=actividad_id, viajero_id=viajero_id).first()
+            actividad_viajero = session.query(ActividadViajero).filter_by(actividad_id=actividad_id,
+                                                                          viajero_id=viajero_id).first()
 
             if actividad is None or viajero is None or actividad_viajero is None:
                 return [False, 'No se encontró el viajero o la actividad.']
             else:
-                if isinstance(concepto, str) and isinstance(anho, int) and isinstance(mes, int) and isinstance(dia, int) and isinstance(monto, (int, float)):
+                if isinstance(concepto, str) and isinstance(anho, int) and isinstance(mes, int) and isinstance(dia,
+                                                                                                               int) and isinstance(
+                        monto, (int, float)):
                     concepto = concepto.strip()
 
                     if concepto != "" and anho > 0 and mes > 0 and dia > 0 and monto > 0:
-                        gastos = session.query(Gasto).filter(Gasto.concepto == concepto, Gasto.actividad_id == actividad_id).all()
-
-                        if len(gastos) == 0:
-                            fecha = date(anho, mes, dia)
-                            monto = round(float(monto), 2)
-                            gasto = Gasto(concepto=concepto, monto=monto, fecha=fecha, viajero_id=viajero_id, actividad_id=actividad_id)
-                            session.add(gasto)
-                            session.commit()
-                            return [True, 'Se añadió con éxito el gasto.']
-                        else:
-                            return [False, 'El concepto está siendo utilizado por un gasto de esta actividad.']
+                        fecha = date(anho, mes, dia)
+                        monto = round(float(monto), 2)
+                        gasto = Gasto(concepto=concepto, monto=monto, fecha=fecha, viajero_id=viajero_id,
+                                      actividad_id=actividad_id)
+                        session.add(gasto)
+                        session.commit()
+                        return [True, 'Se añadió con éxito el gasto.']
                     else:
-                        return [False, 'El concepto no debe estar en blanco, la fecha debe ser coherente y el valor debe ser positivo.']
+                        return [False,
+                                'El concepto no debe estar en blanco, la fecha debe ser coherente y el valor debe ser positivo.']
                 else:
-                    return [False, 'El concepto no debe estar en blanco, la fecha debe ser coherente y el valor debe ser positivo.']
+                    return [False,
+                            'El concepto no debe estar en blanco, la fecha debe ser coherente y el valor debe ser positivo.']
 
     def editarGasto(self, gasto_id, viajero_id, concepto, anho, mes, dia, monto):
         if gasto_id is None or viajero_id is None:
--- tests/test_gasto.py
+++ tests/test_gasto.py
@@ -148,8 +148,6 @@ class ActividadTestCase(unittest.TestCase):
             self.actividad1_id, self.viajero1_id, "Concepto gasto 6", 2020, 12, 12, -4444.43)
         crear_gasto_concepto_en_blanco = self.control_cuenta.crearGastoParaActividad(
             self.actividad1_id, self.viajero1_id, "", 2020, 12, 12, 4444.43)
-        crear_gasto_concepto_repetido = self.control_cuenta.crearGastoParaActividad(
-            self.actividad1_id, self.viajero1_id, self.gasto1_concepto, 2020, 12, 4, 1231.22)
         self.control_cuenta.crearGastoParaActividad(
             self.actividad1_id, self.viajero1_id, self.gasto3_concepto, 2020, 12, 4, 5343.11)
 
@@ -160,7 +158,6 @@ class ActividadTestCase(unittest.TestCase):
         self.assertEqual(False, crear_gasto_concepto_nulo[0])
         self.assertEqual(False, crear_gasto_monto_negativo[0])
         self.assertEqual(False, crear_gasto_concepto_en_blanco[0])
-        self.assertEqual(False, crear_gasto_concepto_repetido[0])
         gasto_creado_con_exito = self.session.query(Gasto).filter(Gasto.actividad_id == self.actividad1_id,
                                                                   Gasto.concepto == self.gasto3_concepto).first()
         self.assertEqual([self.gasto3_concepto, "{:.2f}".format(5343.11)],
