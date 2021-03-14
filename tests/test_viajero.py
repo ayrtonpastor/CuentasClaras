45844dd9fa33a5fbe0124766aa7468c5c602ed07
Ayrton <a.pastor@uniandes.edu.co>
2021-03-14 14:33:24 -0500

    PASO 2: Eliminar viajero asociado a una actividad pero sin gastos (Rojo).

--- tests/test_viajero.py
+++ tests/test_viajero.py
@@ -1,8 +1,12 @@
+import random
 import unittest
 
+from datetime import date
 from faker import Faker
 from src.logica.control_cuenta import ControlCuenta
+from src.modelo.actividad import Actividad, ActividadViajero
 from src.modelo.declarative_base import Session
+from src.modelo.gasto import Gasto
 from src.modelo.viajero import Viajero
 
 
@@ -13,6 +17,11 @@ class ViajeroTestCase(unittest.TestCase):
         self.session = Session()
         """ Generación de datos aleatorios """
         self.data_factory = Faker()
+
+        nombre_actividad1 = self.data_factory.word()
+        self.actividad1 = Actividad(nombre=nombre_actividad1, terminada=False)
+
+        self.session.add_all([self.actividad1])
 
         nombre_viajero1 = self.data_factory.name()
         nombre_viajero2 = self.data_factory.name()
@@ -29,13 +38,29 @@ class ViajeroTestCase(unittest.TestCase):
         self.session.add_all([self.viajero1, self.viajero2, self.viajero3])
         self.session.flush()
 
+        # Viajeros asociados a actividad 1
+        self.actividad_viajero1 = ActividadViajero(actividad_id=self.actividad1.id, viajero_id=self.viajero1.id)
+        self.actividad_viajero2 = ActividadViajero(actividad_id=self.actividad1.id, viajero_id=self.viajero2.id)
+
+        self.session.add_all([self.actividad_viajero1, self.actividad_viajero2])
+
+        # Gasto de viajero 2
+        concepto_gasto1 = self.data_factory.word()
+        monto_gasto1 = round(random.uniform(1000, random.randint(1000, 15000)), 2)
+        fecha_gasto1 = self.data_factory.date_between_dates(date_start=date(2019, 1, 1), date_end=date.today())
+
+        self.gasto1 = Gasto(concepto=concepto_gasto1, monto=monto_gasto1, fecha=fecha_gasto1,
+                            viajero_id=self.viajero2.id, actividad_id=self.actividad1.id)
+        self.session.add_all([self.gasto1])
+
         self.session.commit()
 
         self.viajero1_id = self.viajero1.id
-        self.viajero1_nombre_original = self.viajero1.nombre
         self.viajero2_id = self.viajero2.id
-        self.viajero2_nombre_original = self.viajero2.nombre
         self.viajero3_id = self.viajero3.id
+
+        self.viajero1_nombre_original = self.viajero1.nombre
+        self.viajero2_nombre_original = self.viajero2.nombre
         self.viajero3_nombre_original = self.viajero3.nombre
         self.viajero3_apellido_original = self.viajero3.apellido
 
@@ -44,7 +69,19 @@ class ViajeroTestCase(unittest.TestCase):
     def tearDown(self):
         self.session = Session()
 
+        gastos = self.session.query(Gasto).all()
+        actividad_viajeros = self.session.query(ActividadViajero).all()
+        actividades = self.session.query(Actividad).all()
         viajeros = self.session.query(Viajero).all()
+
+        for gasto in gastos:
+            self.session.delete(gasto)
+
+        for actividad_viajero in actividad_viajeros:
+            self.session.delete(actividad_viajero)
+
+        for actividad in actividades:
+            self.session.delete(actividad)
 
         for viajero in viajeros:
             self.session.delete(viajero)
@@ -103,6 +140,8 @@ class ViajeroTestCase(unittest.TestCase):
     def test_eliminar_viajero(self):
         eliminar_viajero_con_id_nulo = self.control_cuenta.eliminarViajero(None)
         eliminar_viajero_inexistente = self.control_cuenta.eliminarViajero(231)
+        eliminar_viajero_en_actividad_sin_gastos = self.control_cuenta.eliminarViajero(self.viajero1_id)
 
         self.assertEqual(False, eliminar_viajero_con_id_nulo)
         self.assertEqual(False, eliminar_viajero_inexistente)
+        self.assertEqual(False, eliminar_viajero_en_actividad_sin_gastos)
